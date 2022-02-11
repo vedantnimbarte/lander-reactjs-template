2d50306e21431553321bad7f41127dbeb8caf992
Vedant Nimbarte <78648354+vedantnimbarte@users.noreply.github.com>
2022-02-11 22:37:15 +0530

    Removed unused code

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import Logo from "../assets/images/logo.png";
 import "./styles/navbar.css";
 
@@ -6,9 +5,7 @@ function NavBar() {
   return (
     <nav className="navbar">
       <img src={Logo} alt="Logo" className="brand-logo" />
-      <Link to="/">Home</Link>
-      <Link to="/">Landing Pages</Link>
-      <Link to="/404">404</Link>
+
       <a className="contact-btn" href="tel:+9158324958">
         <span>
           <i class="fas fa-phone"></i> &nbsp;
